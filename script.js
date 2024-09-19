bedf63e18e754d27c21e45edb52c2351ff736417
Sudip Kumar <keya24gadu2020@gmail.com>
2024-09-20 02:48:04 +0600

    created home page

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // step1:
 // set Event handeler
-document.getElementById('btn-login').addEventListener('click', function (event) {
+// document.getElementById('btn-login').addEventListener('click', function (event) {
 
     // prevent default behavior(Prevent reloading bowser)
-    event.preventDefault();
-    console.log("login button clicked")
+    // event.preventDefault();
+    // console.log("login button clicked")
 
     // step3:
     // get the phone Number
-    const phone=document.getElementById('phone-number').value
-    console.log(phone)
+//     const phone=document.getElementById('phone-number').value
+//     const pin=document.getElementById('pin-num').value
+//     console.log(phone,pin)
+// })
+
+
+document.getElementById('btn-login').addEventListener('click',function(event){
+event.preventDefault();
+const phonehonenum=document.getElementById('phone-number').value
+const pinnum=document.getElementById('pin-num').value
+
+// wrong way to validate
+if(phonehonenum==='5' && pinnum==='1234'){
+    console.log('login successful');
+    window.location.href='./homePage.html';
+}
+else
+alert('check your number or pin')
 })
