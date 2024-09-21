2c63b7377fe72dccce7a4b3a1424622178f15dec
Sudip Kumar <keya24gadu2020@gmail.com>
2024-09-22 01:59:35 +0600

    implemented cashout methos

--- moneyManage.js
+++ moneyManage.js
@@ -33,3 +33,39 @@ document.getElementById('btn-addMoney').addEventListener('click', function (even
 
 
 })
+
+
+// Cash OUT
+
+
+document.getElementById('btn-reduceMoney').addEventListener('click', function (event) {
+    // Prevent reloading
+    event.preventDefault();
+
+    // step2:
+    // Get money to the aacount balance  and pin number after adding
+    const outMoney = document.getElementById('input-cashOut').value
+    console.log(outMoney)
+    const pin_num = document.getElementById('output-pin').value
+    console.log(pin_num)
+
+    // step3:
+    // verify pin Number
+    if (pin_num === '22**') {
+        console.log('coash out money from your Account');
+        const Current_balance = document.getElementById('balance').innerText
+        console.log(Current_balance)
+            // step4:
+    // get the Corrent balance
+        let new_balance=parseFloat(Current_balance)-parseFloat(outMoney)
+        console.log(new_balance)
+
+        // step5:
+        // update the balance in UI or DOM
+        document.getElementById('balance').innerText=new_balance
+    }
+    else
+        alert('failed to cash out')
+
+
+})
